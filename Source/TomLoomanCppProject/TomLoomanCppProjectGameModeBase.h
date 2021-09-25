338136476cb9eb33355752094d4d52f82ce6b489
Gargowle <84672530+Gargowle@users.noreply.github.com>
2021-09-25 20:02:38 +0200

    Added SCharacter & Setup Input
    - Added Character class
    - Added MoveForward/Turn input
    - Imported starter content provided by Tom Looman

---
+++ Source/TomLoomanCppProject/TomLoomanCppProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TomLoomanCppProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOMLOOMANCPPPROJECT_API ATomLoomanCppProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
