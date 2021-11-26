a7d29c588562a1845f1a7828d4fe5aeade69ffe2
Gargowle <84672530+Gargowle@users.noreply.github.com>
2021-11-26 11:59:34 +0100

    Action and ActionComponent classes (own version of GAS)
    Classes that allow actions to be implemented in own classes rather than in a monolithic character class

--- Source/TomLoomanCppProject/TomLoomanCppProject.Build.cs
+++ Source/TomLoomanCppProject/TomLoomanCppProject.Build.cs
@@ -8,7 +8,7 @@ public class TomLoomanCppProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks", "UMG", "CoreUObject" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
