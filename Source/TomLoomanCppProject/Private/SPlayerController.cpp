2c38d00855aad42c6c7588c3ef72a9af04041d68
Gargowle <84672530+Gargowle@users.noreply.github.com>
2022-01-09 13:48:46 +0100

    Pause menu
    - Create pause menu in SPlayerController
    - New pause menu that allows to jump back into main menu or to continue playing

--- Source/TomLoomanCppProject/Private/SPlayerController.cpp
+++ Source/TomLoomanCppProject/Private/SPlayerController.cpp
@@ -2,6 +2,38 @@
 
 
 #include "SPlayerController.h"
+#include "Blueprint/UserWidget.h"
+
+void ASPlayerController::TogglePauseMenu()
+{
+	if(PauseMenuInstance && PauseMenuInstance->IsInViewport())
+	{
+		PauseMenuInstance->RemoveFromParent();
+		PauseMenuInstance = nullptr; // such that Unreal's garbage collection eventually destroys this
+
+		bShowMouseCursor = false;
+
+		SetInputMode(FInputModeGameOnly());
+		return;
+	}
+
+	PauseMenuInstance = CreateWidget<UUserWidget>(this, PauseMenuClass);
+	if(PauseMenuInstance)
+	{
+		// High ZOrder so it is in front of everything
+		PauseMenuInstance->AddToViewport(100);
+
+		bShowMouseCursor = true;
+		SetInputMode(FInputModeUIOnly());
+	}
+}
+
+void ASPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction(TEXT("PauseMenu"), IE_Pressed, this, &ASPlayerController::TogglePauseMenu);
+}
 
 void ASPlayerController::SetPawn(APawn* InPawn)
 {
