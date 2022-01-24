2247af760a8e666b9f06c97b932b03fe8b4d6087
Gargowle <84672530+Gargowle@users.noreply.github.com>
2022-01-24 20:33:59 +0100

    Enable and configure OnlineSubsystemSteam plugin
    - OnlineSubsystemSteam plugin enabled
    - Boilerplate configuration added to DefaultEngine.ini
    - SteamDevAppId set to the default app value (as it can be used for testing without having generated an ID for this specific app yet)

--- Source/TomLoomanCppProject/TomLoomanCppProject.Build.cs
+++ Source/TomLoomanCppProject/TomLoomanCppProject.Build.cs
@@ -8,7 +8,7 @@ public class TomLoomanCppProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks", "UMG", "CoreUObject", "GameplayTags" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks", "UMG", "CoreUObject", "GameplayTags", "OnlineSubsystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
