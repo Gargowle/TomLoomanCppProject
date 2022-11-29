6917514d38c12792dd8b007fd2a1a043b6f1f0ca
Gargowle <84672530+Gargowle@users.noreply.github.com>
2022-11-29 19:00:46 +0100

    Fix cases in which black hole projectile directly collides with instigator

--- Source/TomLoomanCppProject/Private/SProjectileBase.cpp
+++ Source/TomLoomanCppProject/Private/SProjectileBase.cpp
@@ -65,8 +65,10 @@ void ASProjectileBase::BeginPlay()
 
 void ASProjectileBase::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-
-	Explode();
+	if(OtherActor != GetInstigator())
+	{
+		Explode();		
+	}
 	
 }
 
